10b92893c4c01de431d28d2eab9470de3cb9c541
Pragya Preetam <pragyasabstracts@gmail.com>
2020-11-01 22:26:47 +0530

    merge sorted vectors

--- src/inizio.cpp
+++ src/inizio.cpp
@@ -48,6 +48,22 @@ string rev_str(std::string in_str) {
 	return (in_str);
 }
 
+vector<int> merge_vector(vector<int> vec_1, vector<int> vec_2) {
+	vector<int>::size_type vec_1_len { vec_1.size() },
+			vec_2_len { vec_2.size() }, start_v1 { 0 }, start_v2 { 0 };
+	vector<int> result;
+	while (start_v2 < vec_2_len or start_v1 < vec_1_len) {
+		if (vec_1[start_v1] >= vec_2[start_v2]) {
+			result.push_back(vec_2[start_v2]);
+			start_v2++;
+		} else {
+			result.push_back(vec_1[start_v1]);
+			start_v1++;
+		}
+	}
+	return (result);
+
+}
 int main() {
 	/*vector<Meeting> ou_v;
 	 vector<Meeting> in_v;
@@ -69,6 +85,12 @@ int main() {
 	 << " )";
 	 cout << endl;
 	 }*/
-	cout << "Rev: " << rev_str("");
+	vector<int> myVector { 3, 4, 6, 10, 11, 15 };
+	vector<int> alicesVector { 1, 5, 8, 12, 14, 19 };
+	vector<int> mergedVector = merge_vector(myVector, alicesVector);
+	for (int m : mergedVector) {
+		cout << m << "\t";
+	}
+	//cout << "Rev: " << rev_str("cake pound steal");
 	return (0);
 }
