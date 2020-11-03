94d5569ccbd009b9341a3b5c4b7c5376a36c0993
Pragya Preetam <pragyasabstracts@gmail.com>
2020-11-03 22:51:41 +0530

    strings.

--- src/theory_essentials/pointers.cpp
+++ src/theory_essentials/pointers.cpp
@@ -9,6 +9,12 @@
 Pointers:
 variable_type *pointer_name {nullptr};
 int *int_ptr {nullptr};: Always initialize pointers.
+namespace std {
+template <typename charT,
+typename traits = char_traits<charT>,
+typename Allocator = allocator<charT> >
+class basic_string;
+}
 */
 
 
